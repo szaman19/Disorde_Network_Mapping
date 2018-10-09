3fa68e2761f5941fd2901052c0357395fb40c36d
Shehtab Zaman <szaman5@binghamton.edu>
2018-10-09 14:44:28 -0400

    indexing for lattice spacing

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     cwd = os.getcwd()
     #Pe-1D-Ns100-Aij-disW-0.txt
 
-    file_name = cwd+'/data/Pe-1D-Ns100-Aij-disW-20.txt'
+    file_name = cwd+'/data/Pe-1D-Ns100-Aij-disW-0.txt'
     graph =  generate_graph(file_name)
     #nx.draw(graph, with_labels=True)
     #plt.show()
@@ -44,11 +44,18 @@ def main():
     print(nx.single_source_dijkstra_path(graph,1))
     print(nx.single_source_dijkstra_path_length(graph,1))
     all_path_lengths = dict(nx.all_pairs_dijkstra_path_length(graph))
-    for i in range (1,11):
-        string = ""
-        for j in range(1,11):
-            string += " " + str(all_path_lengths[i][j])
+    average_path = []
+    j = 1
+    i = 1
+    steps = 0
+    while (steps < 11):
+        i = 1
+        j = 1 +steps
+        while(j < 11):
+            print(i,j)
+            i += 1
+            j += 1
 
-        print(string)
+        steps +=1
 
 main()
