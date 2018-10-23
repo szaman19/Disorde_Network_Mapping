7316e37f780f4cdfbd8d45b6e4cfe4bb62909832
Shehtab Zaman <szaman5@binghamton.edu>
2018-10-23 07:34:23 -0400

    updated main.py

--- main.py
+++ main.py
@@ -11,39 +11,44 @@ def generate_graph(file_name):
     
     temp_lst = []
     graph = nx.Graph()
+    label = ''
     for line in data:
         #Clean up the data to get the numerical values
         data_points = line.strip().split()
         data_points = " ".join(data_points).split()
-        #data_points currently hold [from_site_index, to_site_index, correlation_val]
-        site_i = int(data_points[0])
-        site_j = int(data_points[1])
-        if (site_i != site_j):
-            corr = np.reciprocal(float(data_points[2]))
+        if (data_points[0] == 'For'):
+            label = str(data_points[3])
         else:
-            corr = 0
+
+        #data_points currently hold [from_site_index, to_site_index, correlation_val]
+            site_i = int(data_points[0])
+            site_j = int(data_points[1])
+            if (site_i != site_j):
+                corr = np.reciprocal(float(data_points[2]))
+            else:
+                corr = 0
         #Regular division is not helpful in this situation. Make sure to not add when i = j, because corr == inf
 
-        if(site_i != site_j):
+            if(site_i != site_j):
             #print(site_i, site_j, corr)
-            graph.add_edge(site_i,site_j, weight = corr)
+                graph.add_edge(site_i,site_j, weight = corr)
         
     #adj_matrix = nx.adjacency_matrix(graph)
     #print(np.matrix(adj_matrix))
-    return graph
+    return graph, label
 
-def average_path_generator(graph,data_file):
+def average_path_generator(graph,label,data_file):
     all_path_lengths = dict(nx.all_pairs_dijkstra_path_length(graph))
     average_path = []
     j = 1
     i = 1
     steps = 0
-    while (steps < 401):
+    while (steps < 501):
         i = 1
         j = 1 +steps
         counter = 0
         total_length = 0
-        while(j < 401):
+        while(j < 501):
             total_length += all_path_lengths[i][j]
             counter +=1
            # print(i,j)
@@ -53,7 +58,7 @@ def average_path_generator(graph,data_file):
             #print(total_length/ counter)
             average_path.append(total_length/counter)
         steps +=1
-    results = open("Shortest-Paths-"+data_file,"w")
+    results = open("Shortest-Paths-"+label+".txt","w")
     for num in average_path:
         results.write(str(num)+"\n")
 
@@ -61,18 +66,51 @@ def average_path_generator(graph,data_file):
 def main():
     cwd = os.getcwd()
     #Pe-1D-Ns100-Aij-disW-0.txt
-    data_file = 'NoPe-1D-400-Diffusion-Aij-disW-1d6.txt'
+    data_file = 'Pe-1D-500-Diffusion-Aij-disW-0.txt'
     file_name = cwd+'/data/'+data_file
-    graph =  generate_graph(file_name)
+    graph,lab =  generate_graph(file_name)
+    average_path_generator(graph,lab, data_file)
     
-    #data_file1 = 'NoPe-1D-400-Diffusion-Aij-disW-1d3.txt'
-    #file_name1 = cwd+'/data/'+data_file1
-    #graph1 = generate_graph(file_name1)
+    data_file1 = 'Pe-1D-500-Diffusion-Aij-disW-0d0.txt'
+    file_name1 = cwd+'/data/'+data_file1
+    graph1,lab1 = generate_graph(file_name1)
+    average_path_generator(graph1,lab1, data_file1)
 
-    #data_file2 = 'NoPe-1D-400-Diffusion-Aij-disW-1d6.txt' 
-    #file_name2 = cwd+'/data/'+data_file2
-    #graph2 = generate_graph(file_name2)
+    data_file2 = 'Pe-1D-500-Diffusion-Aij-disW-0d1.txt' 
+    file_name2 = cwd+'/data/'+data_file2
+    graph2,lab2 = generate_graph(file_name2)
+    average_path_generator(graph,lab2, data_file2)
 
+    data_file3 = 'Pe-1D-500-Diffusion-Aij-disW-0d2.txt' 
+    file_name3 = cwd+'/data/'+data_file3
+    graph3,lab3 = generate_graph(file_name3)
+    average_path_generator(graph,lab3, data_file3)
+
+    data_file4 = 'Pe-1D-500-Diffusion-Aij-disW-n0d7.txt' 
+    file_name4 = cwd+'/data/'+data_file4
+    graph4,lab4 = generate_graph(file_name4)
+    average_path_generator(graph,lab4, data_file4)
+
+    data_file5 = 'Pe-1D-500-Diffusion-Aij-disW-n0d8.txt' 
+    file_name5 = cwd+'/data/'+data_file5
+    graph5,lab5 = generate_graph(file_name5)
+    average_path_generator(graph,lab5, data_file5)
+
+    data_file6 = 'Pe-1D-500-Diffusion-Aij-disW-n0d9.txt' 
+    file_name6 = cwd+'/data/'+data_file6
+    graph6,lab6 = generate_graph(file_name6)
+    average_path_generator(graph,lab6, data_file6)
+    
+    data_file7 = 'Pe-1D-500-Diffusion-Aij-disW-n1d0.txt' 
+    file_name7 = cwd+'/data/'+data_file7
+    graph7,lab7 = generate_graph(file_name7)
+    average_path_generator(graph,lab7, data_file7)
+    
+    
+    
+    
+    
+    
     #average_path_generator(graph, data_file)
     #average_path_generator(graph1, data_file1)
     #average_path_generator(graph2, data_file2)
@@ -83,20 +121,20 @@ def main():
     #print(nx.single_source_dijkstra_path(graph,1))
     #print(nx.single_source_dijkstra_path_length(graph,1))
 
-    all_path_lengths = dict(nx.all_pairs_dijkstra_path_length(graph))
+    #all_path_lengths = dict(nx.all_pairs_dijkstra_path_length(graph))
     
-    for i in range(1,401,40):
-        lattice_spacing = range(1-i,401-i)
-        path =[]
-        for j in range(1,401):
-            path.append(all_path_lengths[i][j])
+    #for i in range(1,401,40):
+    #    lattice_spacing = range(1-i,401-i)
+    #    path =[]
+    #    for j in range(1,401):
+    #        path.append(all_path_lengths[i][j])
         #path = all_path_lengths[i]
-        label = "Node " + str(i)
-        plt.plot(lattice_spacing, path,label=label)
-    plt.title(data_file.rstrip(".txt"))
-    plt.xlabel("Lattice Spacing")
-    plt.legend()
-    plt.show()
+    #    label = "Node " + str(i)
+    #    plt.plot(lattice_spacing, path,label=label)
+    #plt.title(data_file.rstrip(".txt"))
+    #plt.xlabel("Lattice Spacing")
+    #plt.legend()
+    #plt.show()
 
     #lattice_spacing = range(0,100)
     #fig,ax = plt.subplots()
