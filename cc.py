78795c4a1051b50f22682fb9ed9e8595d656c3cc
Shehtab Zaman <szaman5@binghamton.edu>
2018-12-14 18:10:44 -0500

    Updated cc.py

--- cc.py
+++ cc.py
@@ -3,9 +3,32 @@ import numpy as np
 import scipy 
 import networkx as nx
 import graph_util.py
+from multiprocessing import process
+from multiprocessing import pool as Pool
+from threading import Thread
 import os
 
 def main():
-    files = []
+    files = ['w--0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+            'w-1-0-E-0-diffusion-500.txt',
+
+
+
+
+
+
+
+
+
+            ]
 
 main()
