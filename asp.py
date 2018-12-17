ae62772191d00200ea259fab3039eb053c6a6fb1
Shehtab Zaman <szaman5@binghamton.edu>
2018-12-17 07:32:12 -0500

    Added average paths

--- asp.py
+++ asp.py
@@ -8,18 +8,19 @@ import matplotlib
 import matplotlib.pyplot as plt
 import graph_util
 CWD = os.getcwd()
-FILE_DIR = cwd + "500_Diffusion_data"
+FILE_DIR = CWD + "/500_Diffusion_data/"
+
 def average_shortest_path(file_name, bc = 'periodic'):
-    graph,label = graph_util(FILE_DIR+file_name)
+    graph,label = graph_util.generate_graph(FILE_DIR+file_name)
     all_path_lengths = dict(nx.all_pairs_dijkstra_path_length(graph))
     average_path = []
     if (bc == 'periodic'):
 
-        average_path = [0] * 250
+        average_path = [0] * 251
         for i in range(1,501):
             for k in range(1,501):
                 distance = int(abs(i-k))
-                if(distance > 250):
+                if(distance < 250):
                     index = distance 
                     average_path[index] += all_path_lengths[i][k]
                 else:
@@ -29,30 +30,22 @@ def average_shortest_path(file_name, bc = 'periodic'):
         writer = open(file_name,"w")
         writer.write("W = " + label + '\n')
         for vals in average_path:
-            writer.write(str(num/500)+'\n')
+            writer.write(str(vals/500)+'\n')
         writer.close()
+        return label
+    else:
+        return "invalid argument"
         
+def trial(f):
+    print(f)
 def main():
-    files = ['w-0-E-0-diffusion-500.txt',
-            'w-0-1-E-0-diffusion-500.txt',
-            'w-0-2-E-0-diffusion-500.txt',
-            'w-0-3-E-0-diffusion-500.txt',
-            'w-0-4-E-0-diffusion-500.txt',
-            'w-0-5-E-0-diffusion-500.txt',
-            'w-0-6-E-0-diffusion-500.txt',
-            'w-0-7-E-0-diffusion-500.txt',
-            'w-0-8-E-0-diffusion-500.txt',
-            'w-0-9-E-0-diffusion-500.txt',
-            'w-1-0-E-0-diffusion-500.txt',
-            'w-1-1-E-0-diffusion-500.txt',
-            'w-1-2-E-0-diffusion-500.txt',
-            'Pe-1D-500-Diffusion-Aij-disW-0d1.txt'
-            'w-1-3-E-0-diffusion-500.txt',
-            'w-1-4-E-0-diffusion-500.txt',
-            'w-1-5-E-0-diffusion-500.txt',
-            'Pe-1D-500-Diffusion-Aij-disW-0d1.txt'
-            ]
+    files = ['w-0-E-0-diffusion-500.txt','w-0-1-E-0-diffusion-500.txt','w-0-2-E-0-diffusion-500.txt','w-0-3-E-0-diffusion-500.txt','w-0-4-E-0-diffusion-500.txt','w-0-5-E-0-diffusion-500.txt','w-0-6-E-0-diffusion-500.txt', 'w-0-7-E-0-diffusion-500.txt','w-0-8-E-0-diffusion-500.txt', 'w-0-9-E-0-diffusion-500.txt','w-1-0-E-0-diffusion-500.txt','w-1-1-E-0-diffusion-500.txt','w-1-2-E-0-diffusion-500.txt','Pe-1D-500-Diffusion-Aij-disW-0d1.txt','w-1-3-E-0-diffusion-500.txt','w-1-4-E-0-diffusion-500.txt','w-1-5-E-0-diffusion-500.txt','Pe-1D-500-Diffusion-Aij-disW-0d2.txt']
+    
+    for x in files:
+        print(x)
     pool = Pool.Pool(processes=len(files))
-    results = [pool.apply_async(average_shortest_path, args=(files[x])) for x in range(len(files))]
+    results = [pool.apply_async(average_shortest_path, args=(x,)) for x in files]
     output = [p.get() for p in results]
-    print(output) 
+    print(output)
+
+main()
