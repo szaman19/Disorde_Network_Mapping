a5be55f9b27193a2a445df9b02c48d351cc0d187
Shehtab Zaman <szaman5@binghamton.edu>
2019-02-11 09:42:30 -0500

    changes

--- asp.py
+++ asp.py
@@ -57,7 +57,7 @@ def visualize_paths(files):
         label = 0
         for lines in file_read:
             if (len(lines.split()) > 1):
-                label = "%.2E" % Decimal(lines.split()[2])
+                label = "%.2f" % float(lines.split()[2])
             else:
                 avg_path_data.append(float(lines))
         plt.plot(x_vals,avg_path_data,label=label)
@@ -66,7 +66,22 @@ def visualize_paths(files):
 
 
 def main():
-    files = ['w-0-E-0-diffusion-500.txt','w-0-1-E-0-diffusion-500.txt','w-0-2-E-0-diffusion-500.txt','w-0-3-E-0-diffusion-500.txt','w-0-4-E-0-diffusion-500.txt','w-0-5-E-0-diffusion-500.txt','w-0-6-E-0-diffusion-500.txt', 'w-0-7-E-0-diffusion-500.txt','w-0-8-E-0-diffusion-500.txt', 'w-0-9-E-0-diffusion-500.txt','w-1-0-E-0-diffusion-500.txt','w-1-1-E-0-diffusion-500.txt','w-1-2-E-0-diffusion-500.txt','Pe-1D-500-Diffusion-Aij-disW-0d1.txt','w-1-3-E-0-diffusion-500.txt','w-1-4-E-0-diffusion-500.txt','w-1-5-E-0-diffusion-500.txt']
+    files = ['w-0-E-0-diffusion-500.txt',
+    'w-0-1-E-0-diffusion-500.txt',
+    'w-0-2-E-0-diffusion-500.txt',
+    'w-0-3-E-0-diffusion-500.txt',
+    'w-0-4-E-0-diffusion-500.txt',
+    'w-0-5-E-0-diffusion-500.txt',
+    'w-0-6-E-0-diffusion-500.txt', 
+    'w-0-7-E-0-diffusion-500.txt',
+    'w-0-8-E-0-diffusion-500.txt', 
+    'w-0-9-E-0-diffusion-500.txt',
+    'w-1-0-E-0-diffusion-500.txt',
+    'w-1-1-E-0-diffusion-500.txt',
+    'w-1-2-E-0-diffusion-500.txt',
+    'w-1-3-E-0-diffusion-500.txt',
+    'w-1-4-E-0-diffusion-500.txt',
+    'w-1-5-E-0-diffusion-500.txt']
     visualize_paths(files)
     
 
