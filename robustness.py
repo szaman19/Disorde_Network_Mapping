1b08c1a1036ec09cecbb5449fc12e18d57da6b05
Shehtab Zaman <szaman5@binghamton.edu>
2019-03-12 23:12:00 -0400

    Added robustness and connectivity

--- robustness.py
+++ robustness.py
@@ -6,8 +6,8 @@ import graph_util
 from multiprocessing import process
 from multiprocessing import pool as Pool
 from threading import Thread
-#from gmpy2 import mpfr
-#import gmpy2
+from gmpy2 import mpfr
+import gmpy2
 
 import os
 
@@ -17,12 +17,12 @@ CWD+='/500_Diffusion_data/'
 def per_graph_rb(file_name):
     file_name = CWD + file_name
     graph,label = graph_util.generate_graph(file_name,reciprocal=False)
-    return label + '\t' + str (graph_rb(graph,label))
+    return "W="+label + '\tR=' + str (graph_rb(graph,label))
 
 def graph_rb(graph,label):
     eigen_vals = nx.linalg.spectrum.adjacency_spectrum(graph, weight='weight')
     max_val = 0
-    ret_val = 0#mpfr('0')
+    ret_val = mpfr('0')
 
     for vals in eigen_vals:
         if vals > max_val:
@@ -30,15 +30,14 @@ def graph_rb(graph,label):
     print(max_val)
     eigen_vals = np.array(eigen_vals, dtype=np.float128)
     print(eigen_vals)
-    '''
+    
     for i in range(len(eigen_vals)):
-        eigen_vals[i] = eigen_vals[i]
-        ret_val += gmpy2.exp(eigen_vals[i])
+        ret_val += gmpy2.exp(mpfr(str(eigen_vals[i])))
     ret_val /= 500
     ret_val = gmpy2.log(ret_val)
 
     print(label,str(np.real(ret_val)))
-    '''
+    
     return np.real(ret_val)
 
 def main():
@@ -64,9 +63,11 @@ def main():
     pool = Pool.Pool(processes=len(files))
     results = [pool.apply_async(per_graph_rb, args=(files[x],)) for x in range(len(files))]
     output = [p.get() for p in results]
-    print(output)
+    # for file_name in files:
+    #     output.append(per_graph_rb(file_name))
+    # print(output)
 
-    file_output = open("robustness_per_disorder.txt",'w')
+    file_output = open("robustness_per_disorder.dat",'w')
     for i in output:
         s= i + '\n'
         file_output.write(s)
